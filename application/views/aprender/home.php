7e6704b5d758a03c0e92dd1218952d8c97bcde71
wendelleloi <wendelleloi2@gmail.com>
2019-08-18 04:27:59 -0300

    alterações pagina aprenda/lista

--- application/views/aprender/home.php
+++ application/views/aprender/home.php
@@ -1,9 +1,26 @@
-<div class="container"></div>
+<div class="container">
     <div class="row">
-        <div class="col -xs-12">
-            <h1>
-                sokdsokdsokdos
-            </h1>
+        <div class="col-md-6">
+            <div class="card-aprender">
+                <div class="container-titulo">
+                    <h4><b>Lorem ipsum</b></h4> 
+                </div>
+                <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus alias aspernatur labore laudantium commodi! Debitis, consequuntur necessitatibus, impedit dignissimos natus harum iusto aliquam dolorem assumenda ducimus voluptatem, quisquam similique dolore.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card-aprender">
+                <div class="container-titulo">
+                    <h4><b>Lorem ipsum</b></h4> 
+                </div>
+                <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus alias aspernatur labore laudantium commodi! Debitis, consequuntur necessitatibus, impedit dignissimos natus harum iusto aliquam dolorem assumenda ducimus voluptatem, quisquam similique dolore.</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
