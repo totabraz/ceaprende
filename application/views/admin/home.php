844ce495524a866a6bbdbc0202b2c0443a081b9d
Andressa <kroeffzinha@gmail.com>
2019-08-18 05:07:35 -0300

    tela home

---
+++ application/views/admin/home.php
@@ -0,0 +1,50 @@
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12 col-sm-4">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>2</h3>
+
+                    <p>Assuntos compartilhados</p>
+                </div>
+                <div class="icon">
+                    <i class="glyphicon glyphicon-blackboard"></i>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-xs-12 col-sm-4">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>5</h3>
+
+                    <p>Assuntos aprendidos</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-rocket"></i>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-xs-12 col-sm-4">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>30</h3>
+
+                    <p>Pontuação total</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-trophy"></i>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+    </div>
+
+    <h1>
+        O que deseja fazer hoje?
+    </h1>
+</section>
