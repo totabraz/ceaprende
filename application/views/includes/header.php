99e26b13dfe885b10852ca8a2fc1f3e5497f226a
Andressa <kroeffzinha@gmail.com>
2019-08-18 02:41:45 -0300

    Melhoras no header e nav

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,21 +2,23 @@
     <div class="wrapper">
 
         <header class="main-header">
-            <!-- Logo -->
-           
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
-                <!-- Sidebar toggle button-->
-       
+                <!-- Logo -->
+                <a href="./home" class="logo">
+                    <!-- mini logo -->    
+                    <span class="logo-mini"><b>CeAprende</b></span>    
+                    <!-- logo for regular state and mobile devices -->
+                    <span class="logo"><b>CeAprende</b>Compartilhando e Aprendendo</span>
+                </a>
 
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
-                        
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <img src="<?php echo base_url('dist/img/user2-160x160.jpg'); ?> " class="user-image" alt="User Image">
-                                <span class="hidden-xs">&nbsp;</span>
+                                <span class="hidden-xs">Alexandre Pontes</span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- User image -->
@@ -24,32 +26,14 @@
                                     <img src="<?php echo base_url('dist/img/user2-160x160.jpg'); ?> " class="img-circle" alt="User Image">
 
                                     <p>
-                                        Alexander Pierce - Web Developer
-                                        <small>Member since Nov. 2012</small>
+                                        Alexandre Pontes - Analista de TI
+                                        <small>Setor de Infraestrutura</small>
                                     </p>
-                                </li>
-                                <!-- Menu Body -->
-                                <li class="user-body">
-                                    <div class="row">
-                                        <div class="col-xs-4 text-center">
-                                            <a href="#">Followers</a>
-                                        </div>
-                                        <div class="col-xs-4 text-center">
-                                            <a href="#">Sales</a>
-                                        </div>
-                                        <div class="col-xs-4 text-center">
-                                            <a href="#">Friends</a>
-                                        </div>
-                                    </div>
-                                    <!-- /.row -->
                                 </li>
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
-                                    <div class="pull-left">
-                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                    </div>
                                     <div class="pull-right">
-                                        <a href="<?php echo base_url('logout') ?>" class="btn btn-default btn-flat">Sign out</a>
+                                        <a href="<?php echo base_url('logout') ?>" class="btn btn-default btn-flat">Sair</a>
                                     </div>
                                 </li>
                             </ul>
