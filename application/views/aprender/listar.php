fb59d553227434bc309b081c01f2dc3dc26f4596
wendelleloi <wendelleloi2@gmail.com>
2019-08-18 16:22:07 -0300

    alterações nas paginas de aprender

--- application/views/aprender/listar.php
+++ application/views/aprender/listar.php
@@ -12,28 +12,40 @@
         if (isset($compartilhamentos) && sizeof($compartilhamentos) > 0) {
             for ($i = 0; $i < sizeof($compartilhamentos); $i++) {
                 ?>
-        <a href="<?php echo base_url('aprender/' . $id_categoria .'/' .$compartilhamentos[$i]->ID) ?>" class="col-lg-4 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-yellow"><i class="fa fa-graduation-cap"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text -wrap"><?php echo $compartilhamentos[$i]->titulo ?></span>
-                    <?php
-                            // if (isset($compartilhamentos[$i]->num_assuntos)) {
-                            //     echo '<span class="info-box-number">';
-                            //     echo $compartilhamentos[$i]->num_assuntos . ' pessoas curtiram isso';
-                            //     echo '</span>';
-                            // }
-                            // ?>
-
+            <div class="col-xs-6 col-md-4">
+                <div class="box box-aprender">
+                    <div class="box-header ui-sortable-handle" style="cursor: move;">
+                        <a href="<?php echo base_url('aprender/' . $id_categoria .'/' .$compartilhamentos[$i]->ID) ?>">
+                            <h3 class="box-title"><?php echo $compartilhamentos[$i]->titulo ?></h3>
+                        </a>
+                        <h5>Autor</h5>
+                    </div>
+                    <div class="share-list-screen__card-footer">
+                        <div class="share-list-screen__stats">
+                            <div class="group">
+                                <i class="fa fa-thumbs-up"></i>
+                                <p>13</p>
+                            </div>
+                            
+                        </div>
+                        <div class="share-list-screen__actions">
+                            <button id="aaa" data-id="" type="button" class="btn btn-danger btn-remove">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </a>
+                        <?php
+                                // if (isset($compartilhamentos[$i]->num_assuntos)) {
+                                //     echo '<span class="info-box-number">';
+                                //     echo $compartilhamentos[$i]->num_assuntos . ' pessoas curtiram isso';
+                                //     echo '</span>';
+                                // }
+                                // ?>
+            </div>                    
         <?php
             }
         }
 
         ?>
-
-    </div>
 </section>
