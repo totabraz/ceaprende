0004ecbac49f70a5989364e791bb7c12ef72068f
Izanderson Florencio <izandersonf@gmail.com>
2019-08-18 03:36:45 -0300

    List and registeist and register screens of share flux

---
+++ application/views/compartilhar/cadastrar.php
@@ -0,0 +1,73 @@
+<div class="share-register-screen container">
+  <section class="content-header">
+    <h1>
+    Cadastrar conteúdo
+      <!-- <small>Preview</small> -->
+    </h1>
+  </section>
+  <div class="row">
+    <div class="col-xs-12 col-md-8 share-screen__register-form">
+      <!-- general form elements -->
+      <div class="box box-primary">
+        <!-- <div class="box-header with-border">
+          <h3 class="box-title">Cadastrar conteúdo</h3>
+        </div> -->
+        <!-- /.box-header -->
+        <!-- form start -->
+        <form role="form">
+          <div class="box-body">
+            <div class="form-group">
+              <label>Categoria</label>
+              <select class="form-control">
+                <option>option 1</option>
+                <option>option 2</option>
+                <option>option 3</option>
+                <option>option 4</option>
+                <option>option 5</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Titulo</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            <div class="form-group">
+              <label>Conteúdo</label>
+              <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+            </div>
+            <div class="form-group">
+              <label>Referência</label>
+              <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+            </div>
+            <div class="form-group">
+              <label>Pergunta 1:</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            <div class="form-group">
+              <label>Resposta 1.1:</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            <div class="form-group">
+              <label>Resposta 1.2:</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            <div class="form-group">
+              <label>Resposta 1.3:</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            <div class="form-group">
+              <label>Resposta 1.4:</label>
+              <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+          </div>
+          <!-- /.box-body -->
+
+          <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+      <!-- /.box -->
+
+    </div>
+  </div>
+</div>
