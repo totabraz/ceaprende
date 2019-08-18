ce5ed1452955cfc30c75d6983b23fd70e58dd11a
wendelleloi <wendelleloi2@gmail.com>
2019-08-18 14:40:51 -0300

    atualizações nas paginas de aprender

--- application/views/aprender/listar.php
+++ application/views/aprender/listar.php
@@ -11,14 +11,10 @@
         <?php
         if (isset($compartilhamentos) && sizeof($compartilhamentos) > 0) {
             for ($i = 0; $i < sizeof($compartilhamentos); $i++) {
-                $teste = 0;
-                $cores = ['bg-green', 'bg-yellow', 'bg-red', 'bg-purple', 'bg-blue'];
-                $teste = rand(0, 300) % 5;
-                $teste = $cores[$teste];
                 ?>
         <a href="<?php echo base_url('aprender/' . $id_categoria .'/' .$compartilhamentos[$i]->ID) ?>" class="col-lg-4 col-sm-6 col-xs-12">
             <div class="info-box">
-                <span class="info-box-icon <?php echo $teste; ?>"><i class="fa fa-graduation-cap"></i></span>
+                <span class="info-box-icon bg-yellow"><i class="fa fa-graduation-cap"></i></span>
 
                 <div class="info-box-content">
                     <span class="info-box-text -wrap"><?php echo $compartilhamentos[$i]->titulo ?></span>
